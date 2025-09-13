507ffd4e43683ba5ba1f973e47cb3c02cd3888cc
saraMElbashary <sara.elbashary@gmail.com>
2025-09-13 21:20:53 +0300

    footer creation

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Interface } from './interface/interface';
+import { Footer } from './shared/footer/footer';
 
 @NgModule({
   declarations: [
     App,
     Interface,
+    Footer,
   ],
   imports: [
     BrowserModule,
