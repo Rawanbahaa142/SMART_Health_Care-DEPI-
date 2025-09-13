74a0b580927cc90433c6bad1ad75654a5bf7543c
Rawanbahaa142 <rwanalrawy162@gmail.com>
2025-09-13 20:44:06 +0300

    Banner Section & Create Button Component

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -4,15 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Interface } from './interface/interface';
+import { Button } from './shared/button/button';
 
 @NgModule({
   declarations: [
     App,
     Interface,
+    Button
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
