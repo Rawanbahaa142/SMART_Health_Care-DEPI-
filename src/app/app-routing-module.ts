3e25063bb54e6f34ab0ca2a1bd056437a757fc1c
RanaEmad33 <remad3355@gmail.com>
2025-10-23 17:56:53 +0000

    contact page

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -6,6 +6,7 @@ import { Articles } from './articles/articles';
 import { Services } from './services/services';
 import { Main } from './dashboard/main/main';
 import { Dashboard } from './User/Pages/dashboard/dashboard';
+import { ContactPage } from './contact-page/contact-page';
 
 const routes: Routes = [
 {path:'', component:Interface}  ,
@@ -14,7 +15,8 @@ const routes: Routes = [
 {path:'articles', component:Articles},
 {path:'services' , component:Services},
 {path: 'doctors-dashboard' , component : Main},
-{path:'dashboard' , component:Dashboard}
+{path:'dashboard' , component:Dashboard},
+{path:'contact' , component: ContactPage}
 ];
 
 @NgModule({
